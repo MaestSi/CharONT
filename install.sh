0fc1145cb553a98be53e0e39bf1b695828b732c2
Simone Maestri <simone.maestri@hotmail.it>
2020-01-23 13:56:48 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ make
 ln -s $MINICONDA_DIR"/envs/CharONT_env/racon/build/bin/racon" $MINICONDA_DIR"/envs/CharONT_env/bin"
 conda deactivate
 
-conda create -n pycoQC_env
+conda create -n pycoQC_env pip
 source activate pycoQC_env
 pip install pycoQC
 ln -s $MINICONDA_DIR"/envs/pycoQC_env/bin/pycoQC" $MINICONDA_DIR"/envs/CharONT_env/bin"
