ea20952fd5e8ef2633b83edf346bed17ee9f3cc2
Simone Maestri <simone.maestri@hotmail.it>
2020-06-26 23:39:50 +0200

    Add files via upload

--- CharONT_preprocessing.R
+++ CharONT_preprocessing.R
@@ -307,8 +307,8 @@ if (!dir.exists(d2_preprocessing)) {
         cat(text = paste0("Extracting in-silico PCR products for sample BC", BC_val_curr), file = logfile, sep = "\n", append = TRUE)
         cat(text = paste0("Extracting in-silico PCR products for sample BC", BC_val_curr), sep = "\n")
         system(paste0(CUTPRIMERS, " in=", d2_preprocessing, "/BC", BC_val_curr, "_tmp2.fastq out=", d2_preprocessing, "/BC", BC_val_curr, "_tmp3.fastq sam1=", in_silico_pcr_sam_one, " sam2=", in_silico_pcr_sam_two, " qin=33 fake=f include=t fixjunk"))
-        cat(text = paste0("Now filtering out in-silico PCR products shorter than ", min_seq_length, " for sample BC", BC_val_curr), file = logfile, sep = "\n", append = TRUE)
-        cat(text = paste0("Now filtering out in-silico PCR products shorter than ", min_seq_length, " for sample BC", BC_val_curr), sep = "\n")
+        cat(text = paste0("Now filtering out in-silico PCR products shorter than ", min_seq_length, " bp for sample BC", BC_val_curr), file = logfile, sep = "\n", append = TRUE)
+        cat(text = paste0("Now filtering out in-silico PCR products shorter than ", min_seq_length, " bp for sample BC", BC_val_curr), sep = "\n")
         system(paste0("cat ", d2_preprocessing, "/BC", BC_val_curr, "_tmp3.fastq | ", NANOFILT, " -l ", min_seq_length, " --logfile ", d2_preprocessing, "/BC", BC_val_curr, "_NanoFilt.log > ", d3, "/BC", BC_val_curr, ".fastq"))
         cat(text = "\n", file = logfile, append = TRUE)
       } else {
