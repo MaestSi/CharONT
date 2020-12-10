6800ecbff66786e37787411de096184671294cdc
Simone Maestri <simone.maestri@hotmail.it>
2020-12-10 15:56:47 +0100

    Add files via upload

--- Extract_Samplix_alignments.sh
+++ Extract_Samplix_alignments.sh
@@ -30,14 +30,6 @@ MINIMAP2=minimap2
 SAMTOOLS=samtools
 BEDTOOLS=bedtools
 
-CHR=$(cat $BED | cut -f1)
-FW_START=$(cat $BED | cut -f2)
-FW_END=$(echo $FW_START + 20 | bc)
-RV_END=$(cat $BED | cut -f3)
-RV_START=$(echo $RV_END - 20 | bc)
-FW_PRIMER=$($SAMTOOLS faidx $REFERENCE $CHR":"$FW_START"-"$FW_END | grep -v ">")
-RV_PRIMER=$($SAMTOOLS faidx $REFERENCE $CHR":"$RV_START"-"$RV_END | grep -v ">")
-
 SAMPLE_NAME=$(echo $(basename $READS) | sed 's/\.fast.*//')
 BED_NAME=$(echo $(basename $BED) | sed 's/\.bed//')
 WORKING_DIR=$(realpath $(dirname $READS))
@@ -46,6 +38,7 @@ BAM_target=$WORKING_DIR"/"$SAMPLE_NAME"_"$BED_NAME".bam"
 FASTQ_forward=$WORKING_DIR"/"$SAMPLE_NAME"_"$BED_NAME"_fw.fastq"
 BAM_target_forward=$WORKING_DIR"/"$SAMPLE_NAME"_"$BED_NAME"_fw.bam"
 SAM_target_clipped=$WORKING_DIR"/"$SAMPLE_NAME"_"$BED_NAME"_fw_clipped.sam"
+BAM_target_clipped=$WORKING_DIR"/"$SAMPLE_NAME"_"$BED_NAME"_fw_clipped.bam"
 FASTQ_target_clipped=$WORKING_DIR"/"$SAMPLE_NAME"_"$BED_NAME"_fw_clipped.fastq"
 FASTQ_target_clipped_ids=$WORKING_DIR"/"$SAMPLE_NAME"_"$BED_NAME"_fw_clipped_only_reads_ids.txt"
 FASTQ_extracted=$WORKING_DIR"/"$SAMPLE_NAME"_"$BED_NAME"_extracted.fastq"
@@ -54,20 +47,19 @@ FASTQ_extracted=$WORKING_DIR"/"$SAMPLE_NAME"_"$BED_NAME"_extracted.fastq"
 $MINIMAP2 -ax map-ont --MD $REFERENCE $READS | $SAMTOOLS view -h | $SAMTOOLS sort -o $BAM -T reads.tmp
 $SAMTOOLS index $BAM
 #extract reads overlapping to region of interest
-$BEDTOOLS intersect -a $BAM -b $BED -F 0.9 > $BAM_target
+$BEDTOOLS intersect -a $BAM -b $BED -F 0.99 > $BAM_target
 #extract fastq sequence oriented in forward direction
 $SAMTOOLS view $BAM_target -F260 | awk 'BEGIN {FS="\t"} {print "@" $1"_"$2"_"$3"_"$4"_"$12 "\n" $10 "\n+\n" $11}' > $FASTQ_forward
 #map forward-oriented reads to reference
-$MINIMAP2 -ax map-ont --MD $REFERENCE $FASTQ_forward | $SAMTOOLS view -hSb -F2068 | $BEDTOOLS intersect -a stdin -b $BED -F 0.9 | $SAMTOOLS sort -o $BAM_target_forward -T reads.tmp
+$MINIMAP2 -ax map-ont --MD $REFERENCE $FASTQ_forward | $SAMTOOLS view -hSb -F2068 | $BEDTOOLS intersect -a stdin -b $BED -F 0.99 | $SAMTOOLS sort -o $BAM_target_forward -T reads.tmp
 #clip portions of reads outside of bed
 $PCR_CLIP_READS -B $BED $BAM_target_forward | $SAMTOOLS view -q 1 -hF 4 > $SAM_target_clipped
 #extract clipped and not clipped portions of reads
 $SAM_EXTRACT_CLIP -c $SAM_target_clipped -o $FASTQ_target_clipped
+$SAMTOOLS view -hSb $SAM_target_clipped | $SAMTOOLS sort -o $BAM_target_clipped
 #extract the id of the portions of interest of reads
 cat $FASTQ_target_clipped | grep "clipped" | sed 's/^@//g' > $FASTQ_target_clipped_ids
 #extract the portions of interest of reads
-$SEQTK subseq $FASTQ_target_clipped $FASTQ_target_clipped_ids |  $SEQTK rename - $SAMPLE_NAME"_" > $FASTQ_extracted
-#extract complete reads
-$EXTRACT_AMPLICONS $FASTQ_extracted $FW_PRIMER $RV_PRIMER 0.7
-mkdir tmp
-mv $BAM_target $FASTQ_forward $BAM_target_forward $SAM_target_clipped $FASTQ_target_clipped $FASTQ_target_clipped_ids $FASTQ_extracted "*in_silico_pcr_*" tmp
+$SEQTK subseq $FASTQ_target_clipped $FASTQ_target_clipped_ids | $SEQTK rename - $SAMPLE_NAME"_" > $FASTQ_extracted
+mkdir $WORKING_DIR"/Samplix_reads_extraction"
+mv $BAM_target $FASTQ_forward $BAM_target_forward $SAM_target_clipped $BAM_target_clipped $FASTQ_target_clipped $FASTQ_target_clipped_ids $FASTQ_extracted *in_silico_pcr_* $WORKING_DIR"/Samplix_reads_extraction"
